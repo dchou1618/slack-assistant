77d23fcf505524c84be0176dddbfc9d40a29a71f
dchou1618 <dchou5217@gmail.com>
2026-08-29 12:13:26 -0700

    add generic mention handler running local ollama chat completions model

--- src/slack-assistant/main.py
+++ src/slack-assistant/main.py
@@ -3,16 +3,35 @@ import os
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+import ollama
 
 load_dotenv()
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
-print(os.environ)
 @app.event("app_mention")
-def handle_mention(event, say):
-    print("🔥 RECEIVED:", event)
-    say("Hello! I received your message.")
+def handle_mention(event, say, client):
+    question = event["text"]
+
+    slack_response = say("🧠 Thinking...")
+
+    ollama_response = ollama.chat(
+        model="mistral-small3.1",
+        messages=[
+            {
+                "role": "user",
+                "content": question,
+            }
+        ],
+        keep_alive=-1,
+    )
+
+    answer = ollama_response["message"]["content"]
+    client.chat_update(
+        channel=event["channel"],
+        ts=slack_response["ts"],
+        text=answer,
+    )
 
 
 if __name__ == "__main__":
